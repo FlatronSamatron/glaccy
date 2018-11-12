f01fc34146014795ebe241193d1cdd0c95a498a0
Vitaliy <flatronsamatron@gmail.com>
2018-11-12 11:52:07 +0200

    add block-footer + popup with animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let slides = document.getElementsByClassName('mySlides');
 let dot = document.getElementsByClassName('dot');
-
+let forma = document.getElementById('forma');
 
 function currentSlide(n){
     for(let i = 0; i<slides.length; i++){
@@ -17,5 +17,31 @@ function currentSlide(n){
     }
     if(n == 3){
         document.body.style.backgroundColor ="#9e8c85";
+    }
+}
+
+function formAct() { 
+    let pos = window.pageYOffset-450;
+    let id = setInterval(frame, 1);
+    function frame() {
+      if (pos == window.pageYOffset+100) {
+        clearInterval(id);
+      } else {
+        pos= pos + 10; 
+        forma.style.top = pos + 'px'; 
+      }
+    }
+  }
+
+function formOut(){
+    let pos = window.pageYOffset+100;
+    let id = setInterval(frame, 1);
+    function frame() {
+      if (pos == -450) {
+        clearInterval(id);
+      } else {
+        pos= pos - 10; 
+        forma.style.top = pos + 'px'; 
+      }
     }
 }
