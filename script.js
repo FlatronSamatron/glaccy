f1087750b8a5321b40944520312368175d432a75
Vitaliy <flatronsamatron@gmail.com>
2018-11-14 14:05:17 +0200

    add mobile menu

--- script.js
+++ script.js
@@ -45,3 +45,11 @@ function formOut(){
       }
     }
 }
+
+function openNav(){
+  document.getElementById('mySidenav').style.width =100+'%'
+}
+
+function closeNav(){
+  document.getElementById('mySidenav').style.width =0;
+}
